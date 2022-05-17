9a5233bccf5b5932e51e0cadc844fcdfaba79491
klacsamana15 <klacsamana15@gmail.com>
2022-05-17 11:11:40 -0400

    Style components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,7 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="card bg-secondary border-light text-black p-5">
+        <div class="card-body">
         <Form/>
+        </div>
+        </div>
       </header>
     </div>
   );
--- front-end/src/Components/Form.js
+++ front-end/src/Components/Form.js
@@ -2,17 +2,16 @@ import React from "react";
 
 function Form() {
     return (
-        <form>
-            <div class="mb-3">
-                <h1>TO DO LIST</h1>
-                <input type="text" class="form-control" placeholder="Enter task"/>
-                <div>
+            <div class="card bg-dark border-primary text-white">
+            <div class="card-body">
+            <h5 class="card-title">TO DO LIST</h5>
+                <input type="text" class="form-control my-3" placeholder="Enter task"/>
                 <button class="btn btn-primary mx-3" type="submit">Add</button>
                 <button class="btn btn-primary mx-3" type="submit">Modify</button>
                 <button class="btn btn-primary mx-3" type="submit">Delete</button>
-                </div>
-            </div>
-        </form>
+         </div>
+        </div>
+
     );
 }
 
