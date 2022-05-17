f63f500eff9acf1e1b76d44cc1cd057737a95761
klacsamana15 <klacsamana15@gmail.com>
2022-05-17 14:06:51 -0400

    Add useState

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 import Form from './Components/Form';
+import List from './Components/List';
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  function addTask(taskEntered) {
+      return setTaskList((taskList) => [...taskList, taskEntered]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="card bg-secondary border-light text-black p-5">
         <div class="card-body">
-        <Form/>
+        <Form add={addTask}/>
+        <List view={taskList}/>
         </div>
         </div>
       </header>
--- front-end/src/Components/Form.js
+++ front-end/src/Components/Form.js
@@ -1,18 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Form() {
-    return (
-            <div class="card bg-dark border-primary text-white">
-            <div class="card-body">
-            <h5 class="card-title">TO DO LIST</h5>
-                <input type="text" class="form-control my-3" placeholder="Enter task"/>
-                <button class="btn btn-primary mx-3" type="submit">Add</button>
-                <button class="btn btn-primary mx-3" type="submit">Modify</button>
-                <button class="btn btn-primary mx-3" type="submit">Delete</button>
-         </div>
+function Form(props) {
+
+let AddTask = props.add
+
+const[task, setTask] = useState("");
+
+function taskHandler() {
+    AddTask(task);
+    setTask("");
+}
+
+const submitHandler = (event) => {
+    event.preventDefault();
+}
+
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div class="card bg-dark border-primary text-white mb-3">
+        <div class="card-body">
+          <h5 class="card-title">TO DO LIST</h5>
+          <input
+            type="text"
+            class="form-control my-3"
+            placeholder="Enter task"
+            value={task}
+            onChange={(e) => {setTask(e.target.value)}}
+          />
+          <button 
+          class="btn btn-primary mx-3" 
+          type="button"
+          onClick={taskHandler}>
+            Add
+          </button>
+          <button class="btn btn-primary mx-3" type="button">
+            Modify
+          </button>
+          <button class="btn btn-primary mx-3" type="button">
+            Delete
+          </button>
         </div>
-
-    );
+      </div>
+    </form>
+  );
 }
 
 export default Form;
