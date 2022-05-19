5e2a2c20c95e39fe2f641419339b7c697ca93358
MrSlyP <99894989+MrSlyP@users.noreply.github.com>
2022-05-19 15:55:17 -0400

    Modif App.js Back-end

--- back-end/src/App.js
+++ back-end/src/App.js
@@ -1,18 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
+
 
   //Declare an array of objects "tudoList"
   //initialized to a dummy array
-  const [tudoList, setTudoList] = useState([]);
-
+  //const [tudoList, setTudoList] = useState(["Epicerie","Lavage"]);
+let tudoList =["Epicerie","Lavage"]
   //fonction that received to new tudo and add it to the list
   function addTudo(tudoReceived) {
     let newTudo = tudoReceived;
-    return setTudoList((tudoList) => [
-      ...tudoList,
-      { newTudo },
-    ]);
+    tudoList =  tudoList.push(newTudo)
+    
   }
 
   //function to remove the selected todo
@@ -23,8 +22,16 @@ app.get('/', (req, res) => {
   res.send({tudoList})
 })
 
+app.post('/', (req, res) => {
+  let tudo_ = req.body
+  addTudo(tudo_)
+  res.send(tudoList)
+})
+
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+module.exports = app;
