40b2943c32b63054246a5fff0bfcb92ce8af3fc7
MrSlyP <99894989+MrSlyP@users.noreply.github.com>
2022-05-20 21:28:16 -0400

    tentative de faire communiquer front et bacn-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,26 +4,36 @@ import Form from './Components/Form';
 import List from './Components/List';
 import axios from 'axios';
 
+
 function App() {
   const [taskList, setTaskList] = useState([]);
+
+  /*useEffect ( ()=>{
+    axios.get('http://localhost:3000/')
+  .then(response=>setTaskList(response.data));
+    },[] )*/
+
+    useEffect ( ()=>{
+      fetch('http://localhost:3000/')
+      .then((response)=>{
+        return response.json() //ceci genere une autre promesse donc un autre .then
+    })
+    .then(data =>setTaskList(data));
+      },[] )
 
   function addTask(taskEntered) {
       return setTaskList((taskList) => [...taskList, taskEntered]);
   };
+  
+  
 
-  useEffect(()=>{
-  /*  fetch('http://localhost:3000/tudoList')
-    .then(response => response.json())
-    .then (({tudoList})=>setTaskList(tudoList))
-
-  },[])*/
-
-  (async()=> {
-    const response = await axios.get('http://localhost:3000/tudoList');
+ /* useEffect(()=>{
+    (async()=> {
+    const response = await axios.get('http://localhost:3000/');
     setTaskList(response.data);
   })();
 }
-,[]) 
+,[]) */
 
   function deleteTask(id) {
     const taskToDelete = taskList[id];
