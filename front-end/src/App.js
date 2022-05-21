e293e8697e1fa494041df4325bdf53f41d9919aa
MrSlyP <99894989+MrSlyP@users.noreply.github.com>
2022-05-21 14:40:00 -0400

    Ajout de use effect async dans App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,33 +8,20 @@ import axios from 'axios';
 function App() {
   const [taskList, setTaskList] = useState([]);
 
-  /*useEffect ( ()=>{
-    axios.get('http://localhost:3000/')
-  .then(response=>setTaskList(response.data));
-    },[] )*/
-
-    useEffect ( ()=>{
-      fetch('http://localhost:3000/')
-      .then((response)=>{
-        return response.json() //ceci genere une autre promesse donc un autre .then
-    })
-    .then(data =>setTaskList(data));
-      },[] )
-
-  function addTask(taskEntered) {
-      return setTaskList((taskList) => [...taskList, taskEntered]);
-  };
-  
-  
-
- /* useEffect(()=>{
+  useEffect(()=>{
     (async()=> {
     const response = await axios.get('http://localhost:3000/');
     setTaskList(response.data);
   })();
 }
-,[]) */
+,[]) 
 
+  
+  function addTask(taskEntered) {
+      return setTaskList((taskList) => [...taskList, taskEntered]);
+  };
+  
+  
   function deleteTask(id) {
     const taskToDelete = taskList[id];
     setTaskList(taskList.filter((task) => task !== taskToDelete))
