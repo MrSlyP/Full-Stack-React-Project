f236514c6fc34550bf66570dcc96c30cc2b51da4
MrSlyP <99894989+MrSlyP@users.noreply.github.com>
2022-05-23 10:10:12 -0400

    Add const for links

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,65 +1,61 @@
-import React, {useEffect, useState} from "react";
-import './App.css';
-import Form from './Components/Form';
-import List from './Components/List';
-import axios from 'axios';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Form from "./Components/Form";
+import List from "./Components/List";
+import axios from "axios";
+const link1 = "http://localhost:3000/";
+const link2 = "http://localhost:3000/del";
 
 function App() {
   const [taskList, setTaskList] = useState([]);
 
-  useEffect(()=>{
-    (async()=> {
-    const response = await axios.get('http://localhost:3000/');
-    setTaskList(response.data);
-  })();
-}
-,[]) 
- //inspire by https://stackabuse.com/making-asynchronous-http-requests-in-javascript-with-axios/
-const sendPostRequest = async (taskEntered) => {
-  let sending = {id: taskList.length,task: taskEntered}
-  try {
-      const resp = await axios.post ('http://localhost:3000/',sending);
-      console.log(resp.data);
-  } catch (err) {
-      // Handle Error Here
-      console.error(err);
-  }
-};
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(link1);
+      setTaskList(response.data);
+    })();
+  }, []);
 
-const sendDelRequest = async (taskToDel) => {
-  let delReq = {list: taskToDel}
-  try {
-      const resp = await axios.post ('http://localhost:3000/del',delReq);
+  //inspire by https://stackabuse.com/making-asynchronous-http-requests-in-javascript-with-axios/
+  const sendPostRequest = async (taskEntered) => {
+    let sending = { task: taskEntered };
+    try {
+      const resp = await axios.post(link1, sending);
       console.log(resp.data);
-  } catch (err) {
-      // Handle Error Here
+    } catch (err) {
       console.error(err);
-  }
-};
+    }
+  };
+
+  const sendDelRequest = async (idToDel) => {
+    let delReq = { id: idToDel };
+    try {
+      const resp = await axios.post(link2, delReq);
+      console.log(resp.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   function addTask(taskEntered) {
     sendPostRequest(taskEntered);
     setTaskList((taskList) => [...taskList, taskEntered]);
-    
-}
+  }
 
-  
   function deleteTask(id) {
     const taskToDelete = taskList[id];
-    sendDelRequest(id)
-    setTaskList(taskList.filter((task) => task !== taskToDelete))
-    
+    sendDelRequest(id);
+    setTaskList(taskList.filter((task) => task !== taskToDelete));
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="card bg-secondary border-light text-black p-5">
-        <div className="card-body">
-        <Form add={addTask}/>
-        <List view={taskList} onDelete={deleteTask}/>
-        </div>
+          <div className="card-body">
+            <Form add={addTask} />
+            <List view={taskList} onDelete={deleteTask} />
+          </div>
         </div>
       </header>
     </div>
